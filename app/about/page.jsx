d1b94b6108ee3836937856ed63b71e7a7fe676fd
Esteban <esgduque147@gmail.com>
2023-04-16 20:15:35 -0500

    Aplicación finalizada.

---
+++ app/about/page.jsx
@@ -0,0 +1,21 @@
+function AboutPage() {
+  return (
+    <div>
+        <h1>About</h1>  
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+            Magni nobis dolorem accusamus harum id! Saepe ex consequuntur 
+            adipisci qui vitae laboriosam quas totam harum placeat. Veniam 
+            repellendus dolorum incidunt labore laudantium quibusdam libero 
+            officiis dolor! Culpa quam facere voluptas praesentium deserunt 
+            numquam ratione, quod pariatur fugit hic, aliquam laudantium alias 
+            repellendus libero assumenda modi molestias amet vel quis illo laborum 
+            sed porro itaque. Nesciunt velit ut illo ipsa cupiditate unde suscipit, 
+            est molestias, quibusdam nobis aut perferendis repellat tenetur magni 
+            porro explicabo omnis atque, architecto vero sit quo eius. Atque esse 
+            inventore odio earum fuga, error recusandae sunt at dolores?
+        </p>
+    </div>
+  )
+}
+
+export default AboutPage
---
+++ app/services/page.jsx
@@ -0,0 +1,21 @@
+function ServicesPage() {
+  return (
+    <div>
+        <h1>Services</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+          Nisi sunt distinctio rerum maxime beatae, quisquam voluptas 
+          tenetur unde quo tempora doloremque labore debitis nemo consequatur 
+          laboriosam voluptatum vel? Eius possimus, dicta facere vel aliquam 
+          nesciunt. Sint maxime, veritatis nesciunt itaque veniam quis est. 
+          Fuga illo quae harum architecto assumenda sed, consectetur culpa 
+          impedit voluptates ipsam laboriosam laudantium nihil molestias sequi 
+          adipisci hic ad dolorum! Molestiae, repellendus quam. Ducimus culpa 
+          reiciendis tempore dicta sequi ipsa, aliquid totam suscipit dolore 
+          ipsum ratione delectus veniam ut cumque ab fugit dolores assumenda 
+          pariatur non? Nulla explicabo, tempore nemo obcaecati sint libero! 
+          Qui, obcaecati tempora!</p>
+    </div>
+  )
+}
+
+export default ServicesPage
